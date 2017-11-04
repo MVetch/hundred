dc8f164b4f0d2f5d79aa78a12a9c5c5367320c7f
MV <mishavetchinkin@mail.ru>
2017-11-04 08:26:46 +0300

    alpha 0.1 w/changes

--- main/waitScreen.lua
+++
@@ -1,9 +0,0 @@
-wait = {draw = false}
-
-function wait.show()
-	drawable.button(returnButton)
-	drawMainCircle(getPercent(x, 50), getPercent(y, 30), mainCircle.color)
-	rotate()
-	str = "Searching for the enemy..."
-	love.graphics.print(str, (x - string.width(str, fontSize))*0.5, getPercent(y, 30) + mainCircle.radius + 50)
-end
